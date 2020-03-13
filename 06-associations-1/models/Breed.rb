88a2ec7b0654de9e2513d3f9819db960dc72b025
joshua-miles <joshua@christetulit.org>
2020-03-13 14:45:05 -0500

    -

--- 06-associations-1/models/Breed.rb
+++ 06-associations-1/models/Breed.rb
@@ -1,5 +1,12 @@
 class Breed
 
+    attr_accessor(:name, :average_height)
+
+    def initialize(name, average_height)
+        self.name = name
+        self.average_height = average_height
+    end
+
     def dogs
         my_dogs = []
         Dog.all.each do | dog |
@@ -19,14 +26,5 @@ class Breed
         end
         return my_owners
     end
-
-    attr_accessor(:name, :average_height)
-
-    def initialize(name, average_height)
-        self.name = name
-        self.average_height = average_height
-    end
-
-    
 
 end
