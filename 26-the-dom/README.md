dc455b4c3f988331df96efca0011bb95e993182d
joshua-miles <joshua@christetulit.org>
2020-04-20 11:24:31 -0500

    -

---
+++ 26-the-dom/README.md
@@ -0,0 +1,54 @@
+# The DOM
+
+### Sites you use on a daily basis:
+* Instagram
+* Facebook
+* Gmail
+* YouTube
+* Pinterest
+* Google Maps
+* Amazon
+* Yahoo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Deliverables: 
+
+* Add an Image of a Character to the screen
+* Add an image of a Rock to the screen
---
+++ 26-the-dom/example.js
@@ -0,0 +1,24 @@
+// The DOM 
+//  Document - whole page
+//  Object - Data + Behavior
+//  Model - Represents something (in this case what the user sees)
+
+document = [
+   {
+        "name": "html",
+        "children":[
+            {
+                "name":"body",
+                "children":[
+                    {
+                        "name":"div",
+                        "class":"some-div",
+                        "children":[
+                            "Some Text"
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+]
